a720b7fa1af2e2c314b55bb7eac2ebc52fe1c571
kimyungki <31638902+kimyungki@users.noreply.github.com>
2017-09-06 16:21:07 +0900

    Add files via upload

---
+++ assignment.java
@@ -0,0 +1,10 @@
+package d0905;
+
+public class assignment {
+	public static void main(String args[]){ 
+			System.out.println("your ID number : 2014301017"); 
+			System.out.println("your Name : kim yung ki"); 
+			System.out.println("your mobile pone number : 010-3481-6999"); 
+	 	    System.out.println("your Github address : kimyungki"); 
+		 }	
+}
